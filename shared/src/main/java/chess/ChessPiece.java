5c127d6dfda9aaac969b8cbfac25f54fc6af7660
Rhoopes25 <rachhoopes1@gmail.com>
2025-09-15 15:10:17 -0600

    Biship will now stop when hitting the edge

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -55,7 +56,43 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece piece = board.getPiece(myPosition);
         if (piece.getPieceType() == PieceType.BISHOP) {
-            return List.of(new ChessMove(new ChessPosition(5,2), new ChessPosition(2,5), null));
+            //LIST TO HOLD ALL VALID MOVES INTO IT
+            List<ChessMove> validMoves = new ArrayList<>();
+            int[][] directions = {{1,1}, {1,-1}, {-1,1}, {-1,-1}};
+            for (int[] direction : directions) {
+                int rowDirection = direction[0];
+                int colDirection = direction[1];
+
+                // Start at the NEXT square in this direction
+                int currentRow = myPosition.getRow() + rowDirection;
+                int currentCol = myPosition.getColumn() + colDirection;
+
+                //Now we want to keep moving until we can't
+                while (currentRow >= 1 && currentRow <= 8 && currentCol >= 1 && currentCol <= 8) {
+                    ChessPiece pieceHere = board.getPiece(new ChessPosition(currentRow, currentCol));
+
+                    // is something at next square
+                    ChessPiece pieceAtCurrentSquare = board.getPiece(new ChessPosition(currentRow, currentCol));
+
+                    if (pieceAtCurrentSquare == null){
+                        //We can move here
+                        validMoves.add(new ChessMove(myPosition,  new ChessPosition(currentRow, currentCol), null));
+
+                        // Keep going to next square
+                        currentRow += rowDirection;
+                        currentCol += colDirection;
+                    } else {
+                        // There's a piece here - now what?
+                        // TODO: check if friend or enemy
+                        break; // Stop this direction for now
+
+                    }
+                }
+
+
+                }
+
+
         }
         return List.of();
     }
