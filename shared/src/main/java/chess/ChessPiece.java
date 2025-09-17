0ea1677c14b4fac52c9901801212f233de70f62a
Rhoopes25 <rachhoopes1@gmail.com>
2025-09-17 12:39:08 -0600

    I got Bishop, Queen, and Rook working now working on king

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,9 @@
 package chess;
 
+import chess.pieces.BishopMoves;
+import chess.pieces.QueenMove;
+import chess.pieces.RookMove;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -56,46 +60,14 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece piece = board.getPiece(myPosition);
         if (piece.getPieceType() == PieceType.BISHOP) {
-            //LIST TO HOLD ALL VALID MOVES INTO IT
-            List<ChessMove> validMoves = new ArrayList<>();
-            int[][] directions = {{1,1}, {1,-1}, {-1,1}, {-1,-1}};
-            for (int[] direction : directions) {
-                int rowDirection = direction[0];
-                int colDirection = direction[1];
+            return BishopMoves.getMoves(board, myPosition);
 
-                // Start at the NEXT square in this direction
-                int currentRow = myPosition.getRow() + rowDirection;
-                int currentCol = myPosition.getColumn() + colDirection;
-
-                //Now we want to keep moving until we can't
-                while (currentRow >= 1 && currentRow <= 8 && currentCol >= 1 && currentCol <= 8) {
-                    ChessPiece pieceHere = board.getPiece(new ChessPosition(currentRow, currentCol));
-
-
-                    if (pieceHere == null){
-                        //We can move here
-                        validMoves.add(new ChessMove(myPosition,  new ChessPosition(currentRow, currentCol), null));
-
-                        // Keep going to next square
-                        currentRow += rowDirection;
-                        currentCol += colDirection;
-                    } else {
-                        if (piece.getTeamColor() == pieceHere.getTeamColor()) {
-                            // Same team - can't capture, just stop
-                            break;
-                        } else {
-                            // Enemy piece - can capture it
-                            validMoves.add(new ChessMove(myPosition, new ChessPosition(currentRow, currentCol), null));
-                            break; // Stop after capturing
-                        }
-
-                    }
-                }
-
-
-            }
-            return validMoves;
-
+        }
+        if (piece.getPieceType() == PieceType.ROOK) {
+            return RookMove.getMoves(board, myPosition);
+        }
+        if  (piece.getPieceType() == PieceType.QUEEN) {
+            return QueenMove.getMoves(board, myPosition);
         }
         return List.of();
     }
