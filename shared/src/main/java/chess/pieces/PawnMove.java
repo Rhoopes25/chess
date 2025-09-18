15215f78cd7830f1792365fbd3ea1d0590692f88
Rhoopes25 <rachhoopes1@gmail.com>
2025-09-18 14:52:08 -0600

    working on getting pawn done

--- shared/src/main/java/chess/pieces/PawnMove.java
+++ shared/src/main/java/chess/pieces/PawnMove.java
@@ -9,35 +9,64 @@ public class PawnMove {
 
     public static Collection<ChessMove> getMoves(ChessBoard board, ChessPosition position) {
         ChessPiece piece = board.getPiece(position);
-
         //LIST TO HOLD ALL VALID MOVES INTO IT
         List<ChessMove> validMoves = new ArrayList<>();
-        int[][] directions = {{2,1}, {-2,1}, {2,-1}, {-2,-1}, {1,2}, {-1,2}, {1,-2}, {-1,-2}};
-        for (int[] direction : directions) {
-            int rowDirection = direction[0];
-            int colDirection = direction[1];
 
-            // Start at the NEXT square in this direction
-            int currentRow = position.getRow() + rowDirection;
-            int currentCol = position.getColumn() + colDirection;
+        if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+            int forwardDirection = 1;
 
-            //Now we want to keep moving until we can't
-            if (currentRow >= 1 && currentRow <= 8 && currentCol >= 1 && currentCol <= 8) {
-                ChessPiece pieceHere = board.getPiece(new ChessPosition(currentRow, currentCol));
+            int newRow =  position.getRow() + forwardDirection;
+            int newCol =  position.getColumn();
 
+            if (newRow >= 1 && newRow <= 8) {
+                ChessPiece pieceHere = board.getPiece(new ChessPosition(newRow, newCol));
 
-                if (pieceHere == null || piece.getTeamColor() != pieceHere.getTeamColor()){
-                    //We can move here
-                    validMoves.add(new ChessMove(position,  new ChessPosition(currentRow, currentCol), null));
+                if (pieceHere == null) {
+                    //Add 1 square move
+                    validMoves.add(new ChessMove(position, new ChessPosition(newRow, newCol), null));
 
-
+                    //check if can move two
+                    if (pieceHere == null && position.getRow() == 2) {
+                        int twoSquareRow = position.getRow() + (2 * forwardDirection);
+                        ChessPiece pieceTwoSquares = board.getPiece(new ChessPosition(twoSquareRow, newCol));
+                        if (pieceTwoSquares == null) {
+                            validMoves.add(new ChessMove(position, new ChessPosition(twoSquareRow, newCol), null));
+                        }
+                    }
                 }
-
-
             }
 
 
+        } else {
+            // Black pawn logic
+            int forwardDirection = -1;
+            int newRow =  position.getRow() + forwardDirection;
+            int newCol =  position.getColumn();
+
+            if (newRow >= 1 && newRow <= 8) {
+                ChessPiece pieceHere = board.getPiece(new ChessPosition(newRow, newCol));
+
+
+                if (pieceHere == null) {
+                    //Add 1 square move
+                    validMoves.add(new ChessMove(position, new ChessPosition(newRow, newCol), null));
+
+                    //check if can move two
+                    if (pieceHere == null && position.getRow() == 7) {
+                        int twoSquareRow = position.getRow() + (2 * forwardDirection);
+                        ChessPiece pieceTwoSquares = board.getPiece(new ChessPosition(twoSquareRow, newCol));
+                        if (pieceTwoSquares == null) {
+                            validMoves.add(new ChessMove(position, new ChessPosition(twoSquareRow, newCol), null));
+                        }
+                    }
+                }
+
+            }
         }
+
+
+
+
         return validMoves;
     }
 }
