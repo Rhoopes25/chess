1f4f85da07b6868d48c694afcb98ea0d9cfa986e
Rhoopes25 <rachhoopes1@gmail.com>
2025-09-19 15:02:36 -0600

    Phase 0 done.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -5,6 +5,7 @@ import chess.pieces.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -78,4 +79,19 @@ public class ChessPiece {
         }
         return List.of();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece piece = (ChessPiece) o;
+        return pieceColor == piece.pieceColor && type == piece.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
+    }
 }
+
