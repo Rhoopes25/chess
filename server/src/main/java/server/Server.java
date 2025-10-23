761a091a77f0d61b88fcad3a0688b6a3db3f0960
Rhoopes25 <rachhoopes1@gmail.com>
2025-10-22 19:07:12 -0600

    Add Clear endpoint handler to Server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,11 +3,15 @@ package server;
 import io.javalin.*;
 import dataaccess.*;
 import service.*;
-
+import io.javalin.http.Context;
+import com.google.gson.Gson;
+import java.util.Map;
 
 public class Server {
 
     private final Javalin javalin;
+    // converts java objects into JSON
+    private final Gson gson = new Gson();
 
     // DAOs
     private final UserDAO userDAO = new MemoryUserDAO();
@@ -19,6 +23,22 @@ public class Server {
     private final UserService userService;
     private final GameService gameService;
 
+    // This method will register URL routes with Javalin
+    // like routing
+    private void registerEndpoints() {
+        javalin.delete("/db", this::handleClear);
+    }
+    private Object handleClear(Context ctx) {
+        try {
+            clearService.clear();
+            ctx.status(200);
+            return "{}";
+        } catch (DataAccessException e) {
+            ctx.status(500);
+            return gson.toJson(Map.of("message", e.getMessage()));
+        }
+    }
+
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
@@ -27,12 +47,16 @@ public class Server {
         clearService = new ClearService(userDAO, authDAO, gameDAO);
         userService = new UserService(userDAO, authDAO);
         gameService = new GameService(authDAO, gameDAO);
+
+        registerEndpoints();
     }
+
 
     public int run(int desiredPort) {
         javalin.start(desiredPort);
         return javalin.port();
     }
+
 
     public void stop() {
         javalin.stop();
