646f86cb51517af5bb0f386e13fffec94bc26c7a
Rhoopes25 <rachhoopes1@gmail.com>
2025-10-22 19:12:47 -0600

    Add Register, Login, and Logout endpoint handlers

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,6 +2,10 @@ package server;
 
 import io.javalin.*;
 import dataaccess.*;
+import model.LoginRequest;
+import model.LoginResponse;
+import model.RegisterRequest;
+import model.RegisterResponse;
 import service.*;
 import io.javalin.http.Context;
 import com.google.gson.Gson;
@@ -23,10 +27,15 @@ public class Server {
     private final UserService userService;
     private final GameService gameService;
 
+
     // This method will register URL routes with Javalin
     // like routing
     private void registerEndpoints() {
         javalin.delete("/db", this::handleClear);
+        javalin.post("/user", this::handleRegister);
+        javalin.post("/session", this::handleLogin);
+        javalin.delete("/session", this::handleLogout);
+
     }
     private Object handleClear(Context ctx) {
         try {
@@ -36,6 +45,51 @@ public class Server {
         } catch (DataAccessException e) {
             ctx.status(500);
             return gson.toJson(Map.of("message", e.getMessage()));
+        }
+    }
+    private Object handleRegister(Context ctx) {
+        try {
+            RegisterRequest request = gson.fromJson(ctx.body(), RegisterRequest.class);
+            RegisterResponse response = userService.register(request);
+            ctx.status(200);
+            return gson.toJson(response);
+        } catch (DataAccessException e) {
+            if (e.getMessage().equals("Error: bad request")) {
+                ctx.status(400);
+            } else if (e.getMessage().equals("Error: already taken")) {
+                ctx.status(403);
+            } else {
+                ctx.status(500);
+            }
+            return gson.toJson(Map.of("message", e.getMessage()));
+        }
+    }
+    private Object handleLogin(Context ctx) {
+        try {
+            LoginRequest request = gson.fromJson(ctx.body(), LoginRequest.class);
+            LoginResponse response = userService.login(request);
+            ctx.status(200);
+            return gson.toJson(response);
+        } catch (DataAccessException e) {
+            if (e.getMessage().equals("Error: bad request")) {
+                ctx.status(400);
+            } else if (e.getMessage().equals("Error: unauthorized")) {
+                ctx.status(401);
+            } else {
+                ctx.status(500);
+            }
+            return gson.toJson(Map.of("message", e.getMessage()));
+        }
+    }
+    private Object handleLogout(Context ctx) {
+        try {
+            String authToken = ctx.header("authorization");
+            userService.logout(authToken);
+            ctx.status(200);
+            return "{}";
+        } catch (DataAccessException e) {
+            ctx.status(401);
+            return gson.toJson(Map.of("message", "Error: unauthorized"));
         }
     }
 
