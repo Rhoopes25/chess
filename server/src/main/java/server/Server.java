f22ba8b91b0a7aa70a0aedf848234475477e5891
Rhoopes25 <rachhoopes1@gmail.com>
2025-10-22 18:50:09 -0600

    Initialize DAO instances in Server class

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,10 +1,18 @@
 package server;
 
 import io.javalin.*;
+import dataaccess.*;
+
 
 public class Server {
 
     private final Javalin javalin;
+
+    // DAOs
+    private final UserDAO userDAO = new MemoryUserDAO();
+    private final AuthDAO authDAO = new MemoryAuthDAO();
+    private final GameDAO gameDAO = new MemoryGameDAO();
+
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
